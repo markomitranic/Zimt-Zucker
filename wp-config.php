661d3baa287c21ba45f1576e23a06a03e5d56a57
Marko Mitranic <marko.mitranic@gmail.com>
2016-07-28 14:21:50 +0200

    Added database credentials for server

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'zimt0074');
+define('DB_NAME', 'db639386706');
 
 /** MySQL database username */
 define('DB_USER', 'root');
+// define('DB_USER', 'dbo639386706');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'root');
+// define('DB_PASSWORD', 'jk993RktW0');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
+// define('DB_HOST', 'db639386706.db.1and1.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
